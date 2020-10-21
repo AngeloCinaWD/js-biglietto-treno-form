b32ddb8092587bb7fe59112f1eb918f2d3622570
Angelo <angelo.cina@yahoo.it>
2020-10-21 19:23:30 +0200

    Refactoring ed aggiunta commenti

--- js/myscript.js
+++ js/myscript.js
@@ -2,7 +2,7 @@
 var costoKm = 0.21;
 console.log('COSTOKM', costoKm)
 
-
+// creazione selettori da utilizzare
 var campoTesto = document.getElementById('campoTesto');
 var passeggero = document.getElementById('passeggero');
 var percorrenza = document.getElementById('percorrenza');
@@ -15,13 +15,14 @@ var tuobiglietto = document.getElementById('tuobiglietto');
 
 
 // click bottone Genera biglietto
-
+// creazione selettore bottone genera
 var generabiglietto = document.getElementById('generabiglietto');
-
+// avvenimenti al click del bottone genera
 generabiglietto.addEventListener('click',
 function() {
+  // stampa nome passeggero nel campo specifico del biglietto
   passeggero.innerHTML = campoTesto.value;
-
+  // calcolo costo biglietto ed eventuale sconto da applicare e stampa nel campo del biglietto
   if (fasciaDeta.value === "Sconto Minorenne 20%") {
     costoBiglietto.innerHTML = ((costoKm * parseInt(percorrenza.value)) - ((costoKm * parseInt(percorrenza.value)/100)*20)).toFixed(2) + " &#8364;";
   } else if (fasciaDeta.value === "Sconto Over 65 40%") {
@@ -29,24 +30,24 @@ function() {
   } else {
     costoBiglietto.innerHTML = (costoKm * parseInt(percorrenza.value)).toFixed(2) + " &#8364;";
   }
-
+  // stampa sconto applicato nel campo specifico del biglietto
   sconto.innerHTML = fasciaDeta.value;
-
-  numeroCarrozza.innerHTML = Math.ceil(Math.random() * 10)
-
-  prenotazione.innerHTML = Math.ceil(Math.random() * 100000)
-
-  tuobiglietto.className = ".apparizione"
+  // stampa numero carrozza nel campo specifico del biglietto
+  numeroCarrozza.innerHTML = Math.ceil(Math.random() * 10);
+  // stampa codice prenotazione nel campo specifico del biglietto
+  prenotazione.innerHTML = Math.floor(Math.random() * 10000) + 90000;
+  // far apparire anteprima biglietto
+  tuobiglietto.style.display = "initial";
 
 });
 
 
-// click bottone Annulla
-
+// click input annulla
+// creazione selettore input annulla
 var annulla = document.getElementById('annulla');
-
-
+// avvenimenti al click dell'input annulla
 annulla.addEventListener('click',
 function() {
-  window.location.reload();
+  // far scomparire al reset dei dati inseriti anteprima biglietto
+  tuobiglietto.style.display = "none";
 });
